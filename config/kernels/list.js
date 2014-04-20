396132beee26ab222f101affb5ad79ab22c461b9
sm <smprotocol@gmail.com>
2014-04-20 14:38:50 -0600

    fixing typo

--- config/kernels/list.js
+++ config/kernels/list.js
@@ -4,7 +4,7 @@
 	"description": "Maintains a list of values, where the max length is determined at query time.",
 	"merge": function(key, old, value, log) {
 		return (old||[]).concat(value["value"]).slice(-value["size"]);
-	}, "partial": function(key, left, right, log) {
+	}, "partialMerge": function(key, left, right, log) {
 		return left["value"].concat(right["value"]).slice(-right["size"]);
 	}
 });
